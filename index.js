8001245d645f9023bcbbf4e3f82716e39bbf67d8
yangtandev <jack5542856@gmail.com>
2023-04-07 16:22:22 +0800

    【Feat】Attached a comment on every function.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const ffmpeg = require('fluent-ffmpeg')
 ffmpeg.setFfmpegPath('/usr/local/bin/ffmpeg')
 const options = {
-    key: fs.readFileSync(`/home/ubuntu/certificates/privkey.pem`, 'utf8'),
-    cert: fs.readFileSync(`/home/ubuntu/certificates/fullchain.pem`, 'utf8'),
+    key: fs.readFileSync(`/etc/letsencrypt/live/stream.ginibio.com/privkey.pem`, 'utf8'),
+    cert: fs.readFileSync(`/etc/letsencrypt/live/stream.ginibio.com/fullchain.pem`, 'utf8'),
 }
 const express = require('express')
 const app = express()
@@ -18,10 +18,12 @@ const localhost = isExternalIp
     : require('ip').address()
 const port = 445
 const segmentInSeconds = 300 // 每 5 分鐘擷取一次錄影片段
-
 const RTSPCommands = {}
 const MP4Commands = {}
 
+/* 
+    將原始 RTSP 串流轉換成 Media Server 可接受格式。
+*/
 function RTSPToRTSP(rtsp, type) {
     const output = `rtsp://${localhost}:9554/live/${rtsp.split('@').pop()}`
     const command = getCommand(rtsp)
@@ -78,13 +80,13 @@ function RTSPToRTSP(rtsp, type) {
             })
             .on('error', function (err, stdout, stderr) {
                 console.log(rtsp.split('@').pop(), 'rtsp', err.message)
-                if (err.message == 'Output stream closed') {
-                    RTSPCommands[command].kill()
-                }
-                err = err.message.split(' ').slice(-3, -2).pop()
-                if (err == '404') {
-                    RTSPToRTSP(rtsp, type)
-                }
+                // if (err.message == 'Output stream closed') {
+                //     RTSPCommands[command].kill()
+                // }
+                // err = err.message.split(' ').slice(-3, -2).pop()
+                // if (err == '404') {
+                //     RTSPToRTSP(rtsp, type)
+                // }
             })
             .on('end', function () {
                 delete RTSPCommands[command]
@@ -137,13 +139,13 @@ function RTSPToRTSP(rtsp, type) {
             })
             .on('error', function (err, stdout, stderr) {
                 console.log(rtsp.split('@').pop(), 'rtsp', err.message)
-                if (err.message == 'Output stream closed') {
-                    RTSPCommands[command].kill()
-                }
-                err = err.message.split(' ').slice(-3, -2).pop()
-                if (err == '404') {
-                    RTSPToRTSP(rtsp, type)
-                }
+                // if (err.message == 'Output stream closed') {
+                //     RTSPCommands[command].kill()
+                // }
+                // err = err.message.split(' ').slice(-3, -2).pop()
+                // if (err == '404') {
+                //     RTSPToRTSP(rtsp, type)
+                // }
             })
             .on('end', function () {
                 delete RTSPCommands[command]
@@ -152,6 +154,9 @@ function RTSPToRTSP(rtsp, type) {
     }
 }
 
+/* 
+    截取 Media Server 產生的 MP4 串流，並存於指定位置。
+*/
 function MP4ToMP4(rtsp, is404 = false) {
     const mp4 = `http://${localhost}:9080/live/${rtsp.split('@').pop()}.live.mp4`
     const ip = rtsp.split('@').pop().replace(/\:+/g, '_')
@@ -206,12 +211,12 @@ function MP4ToMP4(rtsp, is404 = false) {
         })
         .on('error', function (err, stdout, stderr) {
             console.log(rtsp.split('@').pop(), 'mp4', err.message)
-            if (err.message == 'Output stream closed') {
-                MP4Commands[command].kill()
-            }
+            // if (err.message == 'Output stream closed') {
+            //     MP4Commands[command].kill()
+            // }
             // err = err.message.split(' ').slice(-3, -2).pop()
             // if (err == '404') {
-            //     MP4ToMP4(rtsp, true)
+            //     MP4ToMP4(rtsp)
             // }
         })
         .on('end', function () {
@@ -220,6 +225,9 @@ function MP4ToMP4(rtsp, is404 = false) {
         .save(`${dir}/${now}.mp4`)
 }
 
+/*  
+    取得指令變數名。
+*/
 function getCommand(rtsp) {
     const command = 'command'
     const ip = rtsp.split('@').pop().match(/\d/g).join('')
@@ -233,27 +241,138 @@ function getCommand(rtsp) {
     return `${command}_${ip}_${now}`
 }
 
+/*  
+    啟動 Media Server。
+*/
+function runMediaServer() {
+    const mediaServer = spawn(
+        `/home/ubuntu/ZLMediaKit/release/linux/Debug/MediaServer -s /home/ubuntu/certificates/ssl.pem`,
+        {
+            shell: true,
+        }
+    )
+
+    mediaServer.stdout.on('data', (data) => {
+        data = `${data}`
+        const err = data.split(' ').slice(7, 8).join(' ')
+
+        // RTSP 斷訊重連
+        if (err == 'RtspSession.cpp:67') {
+            const ip = data
+                .split(' ')
+                .slice(11, 12)
+                .join(' ')
+                .replace(/\)+/g, '/')
+                .split('/')
+                .slice(-2, -1)
+                .join(' ')
+                .match(/\d/g)
+                .join('')
+
+            const rtsp = rtsps.filter((rtsp) => rtsp.split('@').pop().match(/\d/g).join('') == ip).join(' ')
+
+            if (h264_rtsps.includes(rtsp)) {
+                RTSPToRTSP(rtsp, 'h264')
+            } else {
+                RTSPToRTSP(rtsp, 'h265')
+            }
+
+            MP4ToMP4(rtsp, true)
+        }
+    })
+
+    if (h264_rtsps.length > 0) {
+        h264_rtsps.forEach((rtsp) => {
+            RTSPToRTSP(rtsp, 'h264')
+        })
+    }
+
+    if (h265_rtsps.length > 0) {
+        h265_rtsps.forEach((rtsp) => {
+            RTSPToRTSP(rtsp, 'h265')
+        })
+    }
+}
+
+/*  
+    啟動備份機制。
+*/
+function runBackup() {
+    // 定期執行片段備分
+    setInterval(
+        (function backup() {
+            rtsps.forEach((rtsp) => {
+                MP4ToMP4(rtsp)
+            })
+            return backup
+        })(),
+        segmentInSeconds * 1000
+    )
+
+    // 定期清除逾期一個月備份
+    setInterval(
+        (function clearExpiredBackups() {
+            const dir = '/mnt/d/backup/'
+            const expireLimitDays = 1
+            fs.readdir(dir, (err, dates) => {
+                if (err) throw err
+
+                dates.forEach((date) => {
+                    const currentDate = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000)
+                        .toISOString()
+                        .replace(/\:+/g, '-')
+                        .slice(0, 10)
+                    let dateDiff = parseInt(Math.abs(new Date(currentDate) - new Date(date)) / 1000 / 60 / 60 / 24)
+
+                    if (dateDiff > expireLimitDays) fs.rmSync(`${dir}/${date}`, { recursive: true, force: true })
+                })
+            })
+            return clearExpiredBackups
+        })(),
+        1000 * 60 * 60 // 每小時
+    )
+}
+
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
     let containers = ''
 
     rtsps.forEach((rtsp, index) => {
-        containers += `<div id="container${index}"></div>`
+        containers += `<div class="flex"><div class="title">${rtsp
+            .split('@')
+            .pop()}</div><div id="container${index}" class="container"></div></div>`
     })
 
     res.send(` 
     <style type="text/css" scoped>
         body{
-            margin: 0
+            margin: 0;
+            background-color: black;
         }
         body::-webkit-scrollbar {
             display: none;
         }
         .grid {
             display:grid;
-            grid-template-columns: auto auto auto;
+            grid-template-columns: auto auto auto auto auto auto;
             height: 100vh;
+        }
+        .flex{
+            display:flex;
+            flex-direction:column;
+            justify-content: center;    
+            align-items: center;
+            height:33.3vh;   
+        }
+        .title{
+            display:block;
+            font-size: 1.5em;
+            font-weight: bold;
+            color: white;
+            margin: 0.5em 0em 0.5em 0em;
+        }
+        .container{
         }
     </style>
     <script src="./lib/jessibuca-pro.js"></script>
@@ -278,6 +397,9 @@ app.get('/', (req, res) => {
                 loadingTimeoutReplayTimes:-1,
                 heartTimeoutReplayTimes:-1, 
                 autoWasm:true,
+                videoBuffer:0,
+                videoBufferDelay:0.3,
+                useSIMD:true,
                 operateBtns: {
                     fullscreen: false,
                     screenshot: false,
@@ -309,89 +431,13 @@ app.get('/', (req, res) => {
 server.listen(port, () => {
     console.log(`https://${localhost}:${port}`)
 
-    const mediaServer = spawn(
-        `/home/ubuntu/ZLMediaKit/release/linux/Debug/MediaServer -s /home/ubuntu/certificates/ssl.pem`,
-        {
-            shell: true,
-        }
-    )
-
-    mediaServer.stdout.on('data', (data) => {
-        data = `${data}`
-        // console.log(data)
-        const err = data.split(' ').slice(7, 8).join(' ')
-
-        if (err == 'RtspSession.cpp:67') {
-            // 終止當前的串流服務
-            const ip = data
-                .split(' ')
-                .slice(11, 12)
-                .join(' ')
-                .replace(/\)+/g, '/')
-                .split('/')
-                .slice(-2, -1)
-                .join(' ')
-                .match(/\d/g)
-                .join('')
-
-            const rtsp = rtsps.filter((rtsp) => rtsp.split('@').pop().match(/\d/g).join('') == ip).join(' ')
-
-            if (h264_rtsps.includes(rtsp)) {
-                RTSPToRTSP(rtsp, 'h264')
-            } else {
-                RTSPToRTSP(rtsp, 'h265')
-            }
-
-            MP4ToMP4(rtsp)
-        }
-    })
-
-    if (h264_rtsps.length > 0) {
-        h264_rtsps.forEach((rtsp) => {
-            RTSPToRTSP(rtsp, 'h264')
-        })
-    }
-
-    if (h265_rtsps.length > 0) {
-        h265_rtsps.forEach((rtsp) => {
-            RTSPToRTSP(rtsp, 'h265')
-        })
-    }
-
-    // 定期執行片段備分
-    setInterval(
-        (function backup() {
-            rtsps.forEach((rtsp) => {
-                MP4ToMP4(rtsp)
-            })
-            return backup
-        })(),
-        segmentInSeconds * 1000
-    )
-
-    // 定期清除逾期一個月備份
-    setInterval(
-        (function clearExpiredBackups() {
-            const dir = '/mnt/d/backup/'
-            fs.readdir(dir, (err, dates) => {
-                if (err) throw err
-
-                dates.forEach((date) => {
-                    const currentDate = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000)
-                        .toISOString()
-                        .replace(/\:+/g, '-')
-                        .slice(0, 10)
-                    let dateDiff = parseInt(Math.abs(new Date(currentDate) - new Date(date)) / 1000 / 60 / 60 / 24)
-
-                    if (dateDiff > 30) fs.rmSync(`${dir}/${date}`, { recursive: true, force: true })
-                })
-            })
-            return clearExpiredBackups
-        })(),
-        1000 * 60 * 60 // 每小時
-    )
+    runMediaServer()
+    runBackup()
 })
 
+/* 
+    程式中止時，清除相關背景程序。
+*/
 process.on('exit', (code) => {
     console.log('exit')
     server.close()
@@ -400,10 +446,19 @@ process.on('exit', (code) => {
     })
 })
 
+process.on('SIGUSR2', (code) => {
+    console.log('SIGUSR2')
+    server.close()
+    spawn('kill -s 9 `pgrep ffmpeg` && kill -s 9 `pgrep MediaServer`', {
+        shell: true,
+    })
+})
+
+// 使用 CTRL + C 終止程式時，可開啟。
 // process.on('SIGINT', (code) => {
 //     console.log('SIGINT')
 //     server.close()
-//     spawn(`killall ffmpeg MediaServer`, {
+//     spawn('kill -s 9 `pgrep ffmpeg` && kill -s 9 `pgrep MediaServer`', {
 //         shell: true,
 //     })
 // })
