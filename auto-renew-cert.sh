f1b3329535e46f0aa1fdcd5e6830a72658232962
Yang Tan <jack5542856@gmail.com>
2024-04-13 19:21:15 +0930

    【Feat】 Update auto-renew-cert.sh

--- auto-renew-cert.sh
+++ auto-renew-cert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Please add the following command line to /etc/crontab to automatically renew the certbot certificate.
-# 0 0 * * * gini /usr/bin/bash $HOME/NVR/auto-renew-cert.sh
+# Please move this file to your home directory and add the following command line to /etc/crontab to automatically renew the certbot certificate.
+# 0 0 * * * [your username] /usr/bin/bash $HOME/auto-renew-cert.sh
 
 echo 87518499 | sudo -S /usr/bin/certbot renew --nginx > /dev/null 2>&1
 echo 87518499 | sudo -S chown $(whoami) /etc/letsencrypt/live/ -R
