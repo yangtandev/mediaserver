d78ab7d1d551991cdf174ed80053835a4403daeb
Yang Tan <jack5542856@gmail.com>
2024-04-27 07:35:23 +0930

    【Feat】 Remove media-stream-converter.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,6 @@
 module.exports = {
 	apps: [
 		{
-			name: 'media-stream-converter',
-			script: 'media-stream-converter.js',
-			time: true,
-		},
-		{
 			name: 'mediaserver',
 			script: 'mediaserver.js',
 			time: true,
