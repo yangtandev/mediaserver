d7a4563c35bef216ea2afd539421bf4124f5c422
Yang Tan <jack5542856@gmail.com>
2024-06-22 05:30:41 +0930

    【Feat】 Upgrade Nvidia Driver and CUDA versions.

--- setup_mediaserver.sh
+++ setup_mediaserver.sh
@@ -10,17 +10,17 @@ sudo apt-get -y install nvidia-common
 sudo add-apt-repository -y ppa:graphics-drivers
 sudo apt -y update
 sudo ubuntu-drivers devices
-sudo apt -y install nvidia-driver-545
+sudo apt -y install nvidia-driver-555
 
 # Install Cuda
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.3.2/local_installers/cuda-repo-ubuntu2204-12-3-local_12.3.2-545.23.08-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-3-local_12.3.2-545.23.08-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2204-12-3-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo apt-get -y update
-sudo apt-get -y install cuda-toolkit-12-3
-sudo rm -rf cuda-ubuntu2204.pin cuda-repo-ubuntu2204-12-3-local_12.3.2-545.23.08-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/cuda/12.5.0/local_installers/cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-5-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-5
+sudo rm -rf cuda-ubuntu2204.pin cuda-repo-ubuntu2204-12-5-local_12.5.0-555.42.02-1_amd64.deb
 
 # Enter mediaserver directory
 cd $HOME/mediaserver || exit
