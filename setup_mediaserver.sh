486a93c1dd5963557efee854348b6c0770c5dd6a
Yang Tan <jack5542856@gmail.com>
2026-08-24 18:43:49 +0930

    build: harden mediaserver setup script

--- setup_mediaserver.sh
+++ setup_mediaserver.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+set -euo pipefail
 
 # Enter mediaserver directory
-cd $HOME/mediaserver || exit
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # npm install dependencies
-npm i && npm i pm2 -g
+npm i
+npm i -g pm2
 
 # Get the Dependencies
-sudo apt -qq update -y && sudo apt -y install autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libgnutls28-dev libmp3lame-dev libsdl2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev meson ninja-build pkg-config texinfo wget yasm zlib1g-dev ffmpeg
+sudo apt -qq update -y && sudo apt -y install autoconf automake build-essential cmake git-core libass-dev libfreetype6-dev libgnutls28-dev libmp3lame-dev libsctp-dev libsdl2-dev libsrtp2-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev meson ninja-build pkg-config texinfo wget yasm zlib1g-dev ffmpeg
 
 # Enter ZLMediaKit directory
-cd $HOME/mediaserver/ZLMediaKit || exit
+cd ZLMediaKit
 
 # Build and compile the ZLMediaKit
-mkdir build
+mkdir -p build
 cd build
 cmake ..
-make -j4
-cd $HOME/mediaserver || exit
+cmake --build . --parallel "$(nproc)"
+cd ../..
 
 # Running apps with PM2
 pm2 start ecosystem.config.js && pm2 save
